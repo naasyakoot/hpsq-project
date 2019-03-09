e9d86ac579ae21d85c69f39f48ef67f0339a54f6
seum <seumworldinbangladesh@gmail.com>
2019-03-09 15:04:19 +0600

    takes some codes for build db

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
 $servername = "localhost";
 $username = "tester";
-$password = "";
+$password = "1234";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
-    exit("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
