6f377535e2d1b587f88fc68b9d4c7c13e29258cb
seum <seumworldinbangladesh@gmail.com>
2019-03-09 14:45:58 +0600

    connects correctly

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "tester";
+$password = "1234";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
